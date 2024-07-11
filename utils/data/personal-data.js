50b71317596f57a0406e928dbb9e904b3958a145
Gaurav-Zanpure <gauravzanpure211@gmail.com>
2024-07-11 12:42:09 -0700

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Gaurav Zanpure. I am a professional and enthusiastic software engineer with a passion for tackling new challenges and solving complex problems. I am a quick learner with a proactive and self-learning attitude. I love to explore new technologies and continuously improve my skills. I thrive on understanding intricate systems, optimizing performance, and implementing efficient solutions. I am passionate about all aspects of web application development and strive to make the web more open and accessible. My core skills lie in Java, Spring Boot, and Python, but I am always eager to expand my expertise. I am available for any job opportunities that align with my skills and interests. ",
   email: 'zanpuregaurav@gmail.com',
   phone: '+12134776520',
-  address: '1021 W 20th St. ',
+  address: 'CA',
   github: 'https://github.com/Gaurav-Zanpure',
   linkedIn: 'https://www.linkedin.com/in/gaurav-zanpure-188236207/',
   leetcode: "https://leetcode.com/u/zanpuregaurav/",
